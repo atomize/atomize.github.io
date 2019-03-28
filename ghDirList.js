4a47b6e850d25eeea6dc9faa6c6a1801558c665c
berti <63r71@tuta.io>
2019-03-28 14:20:31 -0500

    building directory tree

--- ghDirList.js
+++ ghDirList.js
@@ -38,6 +38,40 @@ const dirFilter = (response, type) => {
     })
     return textContent
 } */
+const createListElement = linkArray => {
+  let listEl = document.createElement("ul");
+  linkArray.map(links => {
+    if (links[1].length == 0) {
+      return listEl.innerHTML += `<li><input type="button" id="${links[0]}">
+                          <label for="${links[0]}">
+                          <a href='/${links[0]}'>${links[0]}</a>
+                          </label></li>
+      `
+    } else {
+      let innerLinks, tempalteCollapse;
+      Promise.all(links[1].map((url,i) =>
+        fetch(url+"/README.md").then(resp => resp.text())
+    )).then(texts => {
+       innerLinks = links[1].map((link,i) => {
+        return `<li><a href="${link}">${converter.makeHtml(texts[i])}</a></li>`
+      })
+    }).then(()=>{
+      
+       tempalteCollapse = `<li><input type="checkbox" id="${links[0]}">
+      <label for="${links[0]}">${links[0]}</label>
+      <ul class="${links[0]}">
+         <li>${innerLinks.join(' ')}</li>
+      </ul></li>`
+      return listEl.innerHTML += tempalteCollapse
+    })
+      
+      
+      
+    }
+  });
+  let wrapper = document.querySelector('.wrapper')
+  wrapper.append(listEl)
+};
 var list = document.getElementById("linkList");
 findGithubRepoContents("atomize", "atomize.github.io")
   .then(arr => dirFilter(arr, "tree"))
@@ -51,28 +85,21 @@ findGithubRepoContents("atomize", "atomize.github.io")
         fetchedPaths.rootPaths[path.path] = [];
       } else {
         let rootDir = path.path.split("/")[0];
-        !fetchedPaths.rootPaths[rootDir]
-          ? (fetchedPaths.rootPaths[rootDir] =
-              [] &&
-              fetchedPaths.rootPaths[rootDir].push(
-                document.location.origin + "/" + path.path
-              ))
-          : fetchedPaths.rootPaths[rootDir].push(
+        !fetchedPaths.rootPaths[rootDir] ?
+          (fetchedPaths.rootPaths[rootDir] = [] &&
+            fetchedPaths.rootPaths[rootDir].push(
               document.location.origin + "/" + path.path
-            );
+            )) :
+          fetchedPaths.rootPaths[rootDir].push(
+            document.location.origin + "/" + path.path
+          );
       }
     }, []);
 
     return Object.entries(fetchedPaths.rootPaths);
   })
-  .then(console.log);
-const createListElement = linkArray => {
-  let listEl = document.createElement("ul");
-  linkArray.map(links => {
-    if (links[1].length == 0) {
-    }
-  });
-};
+  .then(createListElement);
+
 /* const createLinkElement = (linkArray) => {
     let tmplink = ''
 
